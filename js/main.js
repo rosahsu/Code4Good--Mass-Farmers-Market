b2ea58cfae76c74d19e6e97b4f19c675711ce3ad
WideEyedIngenue <esymphony74@gmail.com>
2019-01-23 17:17:33 -0500

    Combined the index.html and map.html

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function createVis() {
 
   //locate the map in the center of massachusetts
   //eventually we should query the user's geolocation to center close to them
-    var MassLatLong=[42.4072, -71.3824]
+    var MassLatLong=[42.4072, -71.3824] //Center of map
 
   // Instantiate visualization object (map area)
-    marketMap = new MapVis("map-area", MassLatLong)
+    marketMap = new makeMap("map-area")
 }
